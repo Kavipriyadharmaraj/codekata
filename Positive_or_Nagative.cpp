8e89f95357a32a94ec7e747680b2c35bfccd4f67
Kavipriyadharmaraj <37578852+Kavipriyadharmaraj@users.noreply.github.com>
2018-03-21 16:24:56 +0530

    Create Positive_or_Nagative.cpp

---
+++ Positive_or_Nagative.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+
+
+using namespace std;
+
+int main()
+
+{
+
+  int k;
+
+  cout<<"Enter your number";
+
+  cin>>k;
+
+  if(k>=1&&k<=100000)
+
+  {
+
+  if(k>0)
+
+  {
+
+      cout<<"It is positive";
+
+}
+
+else
+
+{
+
+    cout<<"It is negative or It may be 0";
+
+}
+
+}
+
+else
+
+{
+
+    cout<<"It is greater than 100000& all are will be positive";
+
+}
+
+
+
+
+
+    return 0;
+
+}
