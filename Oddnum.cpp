ff8e5fd82f83030ee10bc5de61fe5de3522805c3
Kavipriyadharmaraj <37578852+Kavipriyadharmaraj@users.noreply.github.com>
2018-03-24 21:04:11 +0530

    Create Oddnum.cpp

---
+++ Oddnum.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+class Oddnum
+{
+    private:
+    int a,b,i;
+    public:
+    void get()
+    {
+        cout<<"Enter the number";
+        cin>>a>>b;
+        for(i=a;i<=b;i++)
+        { 
+           
+            if(a%2==1)
+            {
+                cout<<a<<"\t";
+            }
+             a=a+1;
+        }
+    }
+
+};
+
+int main()
+{
+ Oddnum o;
+ o.get();
+  
+}
