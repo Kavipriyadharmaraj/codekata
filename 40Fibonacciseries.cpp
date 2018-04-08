080fa296745be713cbc742b88a1af0545146b3b5
Kavipriyadharmaraj <37578852+Kavipriyadharmaraj@users.noreply.github.com>
2018-04-08 19:47:02 +0530

    Create 40Fibonacciseries.cpp

---
+++ 40Fibonacciseries.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+class Fibonacciseries
+{
+    private:
+    int f1=-1,f2=1,f3,i,n;
+    public:
+    void display()
+    {
+        cout<<"Enter the number"<<endl;
+        cin>>n;
+        for(i=0;i<n;i++)
+        {
+            f3=f1+f2;
+            cout<<f3<<"\t";
+            f1=f2;
+            f2=f3;
+        }
+       
+    }
+};
+int main()
+{
+   Fibonacciseries f;
+    f.display();
+}
